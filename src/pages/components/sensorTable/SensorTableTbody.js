d129804010622e0d860f31e2fb8504bf715471b6
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-10-08 19:40:39 +0900

    wjwkd

--- src/pages/components/sensorTable/SensorTableTbody.js
+++ src/pages/components/sensorTable/SensorTableTbody.js
@@ -1,11 +1,44 @@
+import { useState, useRef, useEffect } from 'react';
+
 function SensorTableTbody(sensorTable) {
+  const [statOK, setStatOk] = useState(false);
+  const [hover, setHover] = useState(false);
+
+  const mouseHandler = e => (e === hover ? setHover(false) : setHover(e));
+
+  const batRef = useRef([]);
+
+  useEffect(() => {
+    const batValue = batRef.current;
+    for (let i = 0; i < batValue.length; i++) {
+      const result = parseFloat(batValue[i].innerHTML);
+      console.log(result);
+      if (result === Number) {
+        setStatOk(true);
+      }
+    }
+  });
+
   return (
     <tbody>
       {sensorTable.sensorTableBody.data.map((item, index) => (
-        <tr key={item.thingName}>
+        <tr
+          key={item.thingName}
+          className={hover === index ? 'onHover' : 'onLeave'}
+          onMouseEnter={() => {
+            mouseHandler(index);
+          }}
+        >
           <td className="itemIndex">{index}</td>
           <td>{item.thingName}</td>
-          <td>{item.shadow.batLvl}</td>
+          <td
+            className={statOK ? 'bat_lvl_stat' : 'bat_lvl_stat_low'}
+            ref={item => batRef.current.push(item)}
+
+            // ref={element => (batRef.current[index] = element)}
+          >
+            {item.shadow.batLvl}
+          </td>
           <td>{item.shadow.connectionTime}</td>
           <td>{item.shadow.disconnectionTime}</td>
           <td>{item.shadow.disconnReason}</td>

a92b9d48eaac1f0e279152c17d1d910184b3db2a
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-10-09 01:09:29 +0900

    Add: hover 효과

--- src/pages/components/sensorTable/SensorTableTbody.js
+++ src/pages/components/sensorTable/SensorTableTbody.js
@@ -1,56 +1,37 @@
 import { useState, useRef, useEffect } from 'react';
 
 function SensorTableTbody(sensorTable) {
-  const [statOK, setStatOk] = useState(false);
   const [hover, setHover] = useState(false);
 
   const mouseHandler = e => (e === hover ? setHover(false) : setHover(e));
 
-  const batRef = useRef([]);
-
-  useEffect(() => {
-    const batValue = batRef.current;
-    for (let i = 0; i < batValue.length; i++) {
-      const result = parseFloat(batValue[i].innerHTML);
-      console.log(result);
-      if (result === Number) {
-        setStatOk(true);
-      }
-    }
-  });
-
   return (
     <tbody>
-      {sensorTable.sensorTableBody.data.map((item, index) => (
-        <tr
-          key={item.thingName}
-          className={hover === index ? 'onHover' : 'onLeave'}
-          onMouseEnter={() => {
-            mouseHandler(index);
-          }}
-        >
-          <td className="itemIndex">{index}</td>
-          <td>{item.thingName}</td>
-          <td
-            className={statOK ? 'bat_lvl_stat' : 'bat_lvl_stat_low'}
-            ref={item => batRef.current.push(item)}
-
-            // ref={element => (batRef.current[index] = element)}
+      {sensorTable.sensorTableBody.data.map((item, index) => {
+        return (
+          <tr
+            key={item.thingName}
+            className={hover === index ? 'onHover' : 'onLeave'}
+            onMouseEnter={() => {
+              mouseHandler(index);
+            }}
           >
-            {item.shadow.batLvl}
-          </td>
-          <td>{item.shadow.connectionTime}</td>
-          <td>{item.shadow.disconnectionTime}</td>
-          <td>{item.shadow.disconnReason}</td>
-          <td>{item.shadow.connCardNum}</td>
-          <td>{item.shadow.connectedGateway}</td>
-          <td>{item.shadow.rawSentCnt}</td>
-          <td>{item.shadow.remainData}</td>
-          <td>{item.shadow.rssi}</td>
-          <td>{item.shadow.fwVer}</td>
-          <td>{item.shadow.hwVer}</td>
-        </tr>
-      ))}
+            <td className="itemIndex">{index}</td>
+            <td>{item.thingName}</td>
+            <td>{item.shadow.batLvl}</td>
+            <td>{item.shadow.connectionTime}</td>
+            <td>{item.shadow.disconnectionTime}</td>
+            <td>{item.shadow.disconnReason}</td>
+            <td>{item.shadow.connCardNum}</td>
+            <td>{item.shadow.connectedGateway}</td>
+            <td>{item.shadow.rawSentCnt}</td>
+            <td>{item.shadow.remainData}</td>
+            <td>{item.shadow.rssi}</td>
+            <td>{item.shadow.fwVer}</td>
+            <td>{item.shadow.hwVer}</td>
+          </tr>
+        );
+      })}
     </tbody>
   );
 }

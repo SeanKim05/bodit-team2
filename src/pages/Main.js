2736143e7ae20fd9918271c38a54d3a3cb444748
Bong <2021bongwonny@gmail.com>
2022-10-10 01:47:13 +0900

    Refactor: 불필요한 코드 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import SensorInfoTable from './components/SensorInfoTable';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { faDove } from '@fortawesome/free-solid-svg-icons';
 
 function Main() {
   const [sensorData, setSensorData] = useState([]);
@@ -13,8 +12,6 @@ function Main() {
   const goGraph = () => {
     navigate('/graph');
   };
-  // let url =
-  //   'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a1db46b3-41b2-4a81-b7c6-5f85e7842cca/sensor-info-list.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221007%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221007T132442Z&X-Amz-Expires=86400&X-Amz-Signature=d90a20321e972cc75774c2e445c8e4724ed15f15b99cd1cba82ffa9a4bef1461&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22sensor-info-list.json%22&x-id=GetObject';
   const ColSort = e => {
     setSensorData(
       sensorData.sort((a, b) => {
@@ -75,7 +72,6 @@ function Main() {
 
   useEffect(() => {
     fetch('/bodit-team2/data/SensorInfoList.json')
-      // fetch(url)
       .then(res => res.json())
       .then(data => {
         setSensorData(data.data);
